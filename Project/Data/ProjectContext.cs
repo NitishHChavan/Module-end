eab3491c0991d3628b2c9973ca853de87ae33de5
Nitish <mail2nitishchavan@gmail.com>
2023-07-16 13:53:22 +0530

    Initial commit

---
+++ Project/Data/ProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext (DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Car { get; set; } = default!;
+    }
+}
---
+++ Project/Models/car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Car
+{
+    [Key]
+    public int CarId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public int bhp { get; set; }
+
+    public int cc { get; set; }
+
+    public int FuelTank { get; set; }
+
+    public int FuelEconomy { get; set; }
+}
